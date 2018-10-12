d318bd3a2ef630566688676ccd5a990583edf5ab
Ubuntu <vmadmin123@vmadmin123.tic1irp3uuze1fdxjclk5bcftd.cx.internal.cloudapp.net>
2018-10-12 05:52:15 +0000

    Some changes

--- src/main/java/com/example/demo/WebConfig.java
+++ src/main/java/com/example/demo/WebConfig.java
@@ -9,5 +9,5 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 @ComponentScan(basePackages = "com.example.demo")
 public class WebConfig {
 
-//changes in newbranch
+//changes in newbranches
 }
